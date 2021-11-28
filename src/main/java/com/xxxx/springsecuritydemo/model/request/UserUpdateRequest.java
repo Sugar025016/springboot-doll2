e39f820b4db3be8e6f5387cf807c720d5d03d2ba
Sugar025016 <47650172+Sugar025016@users.noreply.github.com>
2021-11-28 11:49:38 +0800

    first commit

---
+++ src/main/java/com/xxxx/springsecuritydemo/model/request/UserUpdateRequest.java
@@ -0,0 +1,11 @@
+package com.xxxx.springsecuritydemo.model.request;
+
+/**
+ * @author: UserUpdateRequest
+ * @date: 2021/11/22
+ * @description:
+ */
+@Getter
+@Setter
+public class UserUpdateRequest {
+}
